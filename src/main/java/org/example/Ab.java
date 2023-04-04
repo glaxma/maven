d6ac3b825777950d67d90cdc5f44703c67fb023b
laxma <laxmareddy369@gmail.com>
2023-04-04 15:54:10 +0530

    made method to void

--- src/main/java/org/example/Ab.java
+++ src/main/java/org/example/Ab.java
@@ -3,14 +3,13 @@ package org.example;
 import java.util.ArrayList;
 
 public class Ab {
-    public static ArrayList names() {
-        ArrayList al = new ArrayList<>();
+    public static void names() {
+        ArrayList<String> al = new ArrayList<>();
         al.add("rahul");
         al.add("suresh");
         al.add("hero");
         al.add("zero");
         System.out.println(al);
-        return al;
     }
 
     public static void main(String[] args) {
